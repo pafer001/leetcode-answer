b3e64c40756e70e284daf4beb5f888939c09b4af
wangzhenya <wangzhenya@zhuanzhuan.com>
2021-03-07 10:12:27 +0800

    1

--- src/main/java/com/offer/answer/_12/MinArray.java
+++ src/main/java/com/offer/answer/_12/MinArray.java
@@ -1,6 +1,13 @@
 package com.offer.answer._12;
 
+
 public class MinArray {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 3, 3};
+        final int i = new MinArray().minArray(numbers);
+        System.out.println(i);
+    }
 
     public int minArray(int[] numbers) {
 
@@ -8,19 +15,45 @@ public class MinArray {
         int right = numbers.length - 1;
 
         while (right > left) {
+            if (right - left == 1) {
+                return min(numbers[left], numbers[right]);
+            }
             int mid = (left + right) / 2;
 
             if (numbers[left] > numbers[mid]) {
                 right = mid;
-//            mid = (left + right)/2;
             }
             if (numbers[right] < numbers[mid]) {
-                right = mid;
-//            mid = (left + right)/2;
+                left = mid;
+            }
+
+            if (numbers[left] == numbers[right] || numbers[left] == numbers[mid]) {
+                int small = numbers[left];
+                for (int i = left; i <= right; i++) {
+
+                    if (numbers[i] < small) {
+                        small = numbers[i];
+                    }
+                }
+
+                return small;
+            }
+
+            if (numbers[left] <= numbers[mid] && numbers[right] >= numbers[mid]) {
+                return numbers[left];
             }
         }
         return numbers[left];
 
 
     }
+
+    private int min(int a, int b) {
+
+        if (a > b) {
+            return b;
+        }
+        return a;
+    }
+
 }
